e187a976c8d6a42ad04026dfaf7396f5303de594
ingleonelrv <ing.leonelrv@gmail.com>
2018-11-23 18:48:48 -0600

    generando secuencia random de colores

--- js/code.js
+++ js/code.js
@@ -7,12 +7,24 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego {
     constructor() {
         this.inicializar()
+        this.generarSecuencia()
     }
 
     inicializar() {
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+    generarSecuencia(){
+        //declaramos un array de 10 elementos y le asignamos 0 a c/u para luego mapearlo con aleatorios de 0-3 q representan 1color
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
     }
 }
 function empezarJuego() {
-    var juego = new Juego()
+    window.juego = new Juego()
 }
