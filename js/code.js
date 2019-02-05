ea801e14202bcdbe820b98550adbfcab7900d454
ingleonelrv <ing.leonelrv@gmail.com>
2019-02-05 13:09:00 -0600

    Probando positionCSS

--- js/code.js
+++ js/code.js
@@ -7,6 +7,9 @@ const ULTIMO_NIVEL =10
 
 class Juego {
     constructor() {
+        //MUY IMPORTANTE BIND AL INICIO Y NO SOBRE LA MARCHA CONSTANTEMENTE, DA PROBLEMAS
+        this.elegirColor=this.elegirColor.bind(this)
+        this.inicializar = this.inicializar.bind(this)
         this.inicializar()
         this.generarSecuencia()
         setTimeout(this.siguienteNivel, 500)
@@ -16,7 +19,7 @@ class Juego {
     inicializar() {
         //corrigiendo la salida de contexto de this provocada por setTimeOut
         this.siguienteNivel = this.siguienteNivel.bind(this)
-        btnEmpezar.classList.add('hide')
+        this.toggleBtnEmpezar()
         this.nivel = 1
         //array de los botones de cada color
         this.colores = {
@@ -24,6 +27,13 @@ class Juego {
             violeta,
             naranja,
             verde
+        }
+    }
+    toggleBtnEmpezar(){
+        if (btnEmpezar.classList.contains('hide')) {
+            btnEmpezar.classList.remove('hide')          
+        }else{
+            btnEmpezar.classList.add('hide')     
         }
     }
     generarSecuencia(){
@@ -64,8 +74,10 @@ class Juego {
         for (let i = 0; i < this.nivel; i++) {
             //let mantiene el valor para el for, var lo sobreescribe
             let color = this.transformarNumeroAColor(this.secuencia[i])
+            // console.log(color)
             //este delay es porque el for se ejecuta muy rapido y no daria tiempo de ver la iluminacion
             setTimeout(() => {
+                console.log(color)
                 this.iluminarColor(color)
             }, 1000 * i)
         }
@@ -84,16 +96,16 @@ class Juego {
         // .bind(this) ata la referencia al juego ya que en el camino se puede perder el contexto y la trasladaria al boton
         // var self = this o var _this = this
         // this.elegirColor = this.elegirColor.bind(this) inicializado en el constructor no necesitaria bind en cada elemento
-        this.colores.celeste.addEventListener('click', this.elegirColor.bind(this))
-        this.colores.violeta.addEventListener('click', this.elegirColor.bind(this))
-        this.colores.naranja.addEventListener('click', this.elegirColor.bind(this))
-        this.colores.verde.addEventListener('click', this.elegirColor.bind(this))
+        this.colores.celeste.addEventListener('click', this.elegirColor)
+        this.colores.violeta.addEventListener('click', this.elegirColor)
+        this.colores.naranja.addEventListener('click', this.elegirColor)
+        this.colores.verde.addEventListener('click', this.elegirColor)
     }
     eliminarEventosClick(){
-        this.colores.celeste.removeEventListener('click', this.elegirColor.bind(this))
-        this.colores.violeta.removeEventListener('click', this.elegirColor.bind(this))
-        this.colores.naranja.removeEventListener('click', this.elegirColor.bind(this))
-        this.colores.verde.removeEventListener('click', this.elegirColor.bind(this))  
+        this.colores.celeste.removeEventListener('click', this.elegirColor)
+        this.colores.violeta.removeEventListener('click', this.elegirColor)
+        this.colores.naranja.removeEventListener('click', this.elegirColor)
+        this.colores.verde.removeEventListener('click', this.elegirColor)  
     }
     elegirColor(event){
         const nombreColor = event.target.dataset.color
@@ -103,17 +115,32 @@ class Juego {
             this.subNivel++
             if (this.subNivel === this.nivel) {
                 this.nivel++
-                // this.eliminarEventosClick()
+                this.eliminarEventosClick()
                 if (this.nivel === (ULTIMO_NIVEL + 1)) {
-                    //GANO
+                    this.ganoElJuego()
                 }else{
                     //el setTimeOut delega en el navegador la ejecucion de la funcion, el this = window, usar bind para que this sea this
-                    setTimeout(this.siguienteNivel, 1000)                    
+                    setTimeout(this.siguienteNivel, 1500)                
                 }
             }
         }else{
-            //PERDIO
+            this.perdioElJuego()
         }
+    }
+    ganoElJuego(){
+        //swal devuelve una promesa
+        swal("Simon Dice", "Felicidades, ganaste el juego!", "success")
+            .then(()=> {
+                this.inicializar()
+            })
+    }
+    perdioElJuego(){
+        swal("Simon Dice", "Lo siento, perdiste :(", "error")
+            .then(()=> {
+                this.eliminarEventosClick()
+                this.inicializar()
+            })
+            // debugger
     }
 }
 function empezarJuego() {
